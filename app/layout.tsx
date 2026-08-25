55e199d2e21f873481c3cc8957f0eacde4e1e0dd
brunohop76-glitch <brunohop76@gmail.com>
2026-08-25 10:35:13 -0300

    Load final Pokemon detail visual polish

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import "./pokemon-enhancements.css";
 import "./pokemon-detail-enhancements.css";
 import "./pokemon-detail-fix.css";
 import "./pokemon-detail-hero-reset.css";
+import "./pokemon-detail-polish.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
